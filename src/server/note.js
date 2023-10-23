7da4973478203aade6dbda9f25fcecb0b5731483
FENG <982685807@qq.com>
2023-10-23 17:25:44 +0800

    update

---
+++ src/server/note.js
@@ -0,0 +1,31 @@
+import axios from "axios"
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+}
+
+const create = person => {
+    const request = axios.post(baseUrl,person)
+    return request.then(response => response.data)
+}
+
+const deletePerson = person => {
+    const request = axios.delete(`${baseUrl}/${person.id}`,{data:{person}})
+    .then(console.log('delete success'))
+    .catch(error => {console.log(error)})
+    return request.then(response => response.data)
+}
+
+const update = person => {
+    console.log(person)
+    const request = axios.put(`${baseUrl}/${person.id}`,person)
+    .catch(error => {
+        console.log(error)
+    })
+    return request.then(response => response.data)
+}
+
+export default { getAll,create,deletePerson,update }
